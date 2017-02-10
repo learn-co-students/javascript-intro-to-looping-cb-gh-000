147f5beff99e1b90355db8301747952c56ddf6a7
Keith Gordon <keithg07@gmail.com>
2017-02-10 18:17:04 +0000

    first change

---
+++ loops.js
@@ -0,0 +1,9 @@
+function forLoop() {
+  for(i=0 ; i <= 25; i++) {
+    if( i === 1) {
+      console.log("I am 1 strong loop");
+    } else if() {
+      console.log("I am ${i} strong loop");
+    }
+  }
+}
