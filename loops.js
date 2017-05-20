8c1e7991be528f4df45d6fea4bf3144fc1ea5181
Jason Anders <k1463801@kcl.ac.uk>
2017-05-20 20:00:04 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,17 +1,26 @@
-function forLoop(array){
-  for (var i = 1; i < 26; i++) {
-    array.push(`I am ${i} strange loops.`)
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
   }
+
   return array
 }
 
-function whileLoop(n){
+
+function whileLoop(n) {
   while (n > 0) {
     console.log(--n)
+  }
+
+  return 'done'
 }
 
-function doWhileLoop(array){
-  function maybeTrue(){
+function doWhileLoop(array) {
+  function maybeTrue() {
     return Math.random() >= 0.5
   }
 
