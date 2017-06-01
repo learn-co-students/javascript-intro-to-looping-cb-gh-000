70b09b5381f8b8d609150a2f9869df2032bb04b8
Robert Wayne <robertjwayne@gmail.com>
2017-06-01 19:16:43 -0400

    Create function doWhileLoop

--- loops.js
+++ loops.js
@@ -16,3 +16,15 @@ function whileLoop(n) {
     }
     return "done";
 }
+
+function maybeTrue() {
+    return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+    do {
+        array.pop();
+    } while (maybeTrue() && array.length > 0);
+
+    return array;
+}
