a0f5ed68b84e85b88b331ca5c13f57284ccb7c98
Jordan de Souza <ja3desou@uwaterloo.ca>
2019-06-07 15:26:37 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,11 @@
 function forLoop(array){
   for(let i = 0; i < 25; i++){
-    array[i] = `"I am ${i} strange loop${i === 0 ? '' : 's'}."`
+    if(i === 1){
+      array[1] = "I am 1 strange loop.";
+    }
+    else{
+      array[i] = `I am ${i} strange loop${i === 0 ? '' : 's'}.`
+    }
   }
   return array
 }
