042b6262abb58f5d9ab1fb657d10c3aef85d3e3d
Jordan de Souza <ja3desou@uwaterloo.ca>
2019-06-07 15:43:44 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,3 +17,16 @@ function whileLoop(number){
   }
   return "done"
 }
+
+
+function doWhileLoop(num){
+  var i = 0;
+
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+  do{
+    console.log("I run once regardless.");
+  }while(incrementVariable < num);
+}
