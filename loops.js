0bac0ba6ac7405c827ea9bc879aacb88c59a78a2
Rohit Kale <ruk22@cam.ac.uk>
2020-04-30 22:55:30 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,28 @@
+function forLoop(arr) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      arr.push(`I am 1 strange loop.`);
+    }
+    else {
+      arr.push(`I am ${i} strange loops.`);
+    }
+  }
+  return arr
+}
+
+function whileLoop (n) {
+  while (n>0) {
+    console.log(--n)
+  }
+  return "done"
+}
+
+function doWhileLoop (i) {
+  function incrementVariable() {
+    i = i - 1;
+    return i;
+  }
+    do {
+    console.log("I run once regardless.");
+  } while (incrementVariable() > 0);
+}
